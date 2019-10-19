36d689fe46b70e5a260405d61ce0decdf0e4a3f5
James Messinger <james.messinger@gmail.com>
2019-10-19 08:29:06 -0500

    Added tests for package exports

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { createFile } from "./create-file";
+export { IterableWriter } from "./iterable-writer";
+export { iterate } from "./iterate";
+export { iterateAll } from "./iterate-all";
+export { joinIterables } from "./join-iterables";
+export { log } from "./log";
+export { splitIterable } from "./split-iterable";
+export { validate } from "./validate";
---
+++ test/specs/exports.spec.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const { expect } = require("chai");
+const commonJSExport = require("../../");
+const {
+  default: defaultExport, createFile, log, validate,
+  iterate, iterateAll, joinIterables, splitIterable, IterableWriter
+} = require("../../");
+
+describe("@code-engine/utils package exports", () => {
+
+  it("should not have a default ESM export", () => {
+    expect(defaultExport).to.be.equal(undefined);
+  });
+
+  it("should export the createFile function as a named export", () => {
+    expect(createFile).to.be.a("function");
+    expect(createFile.name).to.equal("createFile");
+  });
+
+  it("should export the log function as a named export", () => {
+    expect(log).to.be.a("function");
+    expect(log.name).to.equal("log");
+  });
+
+  it("should export the iterate function as a named export", () => {
+    expect(iterate).to.be.a("function");
+    expect(iterate.name).to.equal("iterate");
+  });
+
+  it("should export the iterateAll function as a named export", () => {
+    expect(iterateAll).to.be.a("function");
+    expect(iterateAll.name).to.equal("iterateAll");
+  });
+
+  it("should export the joinIterables function as a named export", () => {
+    expect(joinIterables).to.be.a("function");
+    expect(joinIterables.name).to.equal("joinIterables");
+  });
+
+  it("should export the splitIterable function as a named export", () => {
+    expect(splitIterable).to.be.a("function");
+    expect(splitIterable.name).to.equal("splitIterable");
+  });
+
+  it("should export the IterableWriter class as a named export", () => {
+    expect(IterableWriter).to.be.a("function");
+    expect(IterableWriter.name).to.equal("IterableWriter");
+  });
+
+  it("should export the validate object as a named export", () => {
+    expect(validate).to.be.an("object").with.keys("concurrency");
+    expect(validate.concurrency).to.be.a("function");
+  });
+
+  it("should not export anything else", () => {
+    expect(commonJSExport).to.have.keys(
+      "createFile",
+      "log",
+      "iterate",
+      "iterateAll",
+      "joinIterables",
+      "splitIterable",
+      "IterableWriter",
+      "validate",
+    );
+  });
+
+});
