8afde40c5b5f17900b0f2e3e478940af2fb30110
James Messinger <james.messinger@gmail.com>
2019-11-06 07:51:10 -0600

    Split `valuesToString()` into two functions that can be used independently

--- src/values-to-string.ts
+++ src/values-to-string.ts
@@ -5,16 +5,23 @@ import { valueToString, ValueToStringOptions } from "./value-to-string";
  */
 export function valuesToString(values: unknown[], options: ValueToStringOptions = {}): string {
   let stringValues = values.map((value) => valueToString(value, options));
+  return humanizeList(stringValues, options);
+}
+
+/**
+ * Returns a list of strings as a comma separated string, like "one, two, three and four".
+ */
+export function humanizeList(values: string[], options: ValueToStringOptions): string {
   let lastValue = "", oxfordComma = "";
 
   if (values.length > 1) {
     let conjunction = options.conjunction || "and";
-    lastValue = ` ${conjunction} ${stringValues.pop()!}`;
+    lastValue = ` ${conjunction} ${values.pop()!}`;
   }
 
-  if (values.length > 2) {
+  if (values.length > 1) {
     oxfordComma = ",";
   }
 
-  return stringValues.join(", ") + oxfordComma + lastValue;
+  return values.join(", ") + oxfordComma + lastValue;
 }
