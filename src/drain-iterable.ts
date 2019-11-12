d3478e970edf79b22aa65cd5aa7f67b838af136b
James Messinger <james.messinger@gmail.com>
2019-11-12 08:16:41 -0600

    Added a `concurrency` argument to `drainIterable()` to allow it to read multiple values at a time

--- src/drain-iterable.ts
+++ src/drain-iterable.ts
@@ -1,14 +1,21 @@
+import { ConcurrentTasks } from "./concurrent-tasks";
 import { demandIterator } from "./get-iterator";
 
 /**
  * Iterates over all values in an async iterable to trigger any side-effects of reading each value.
  */
-export async function drainIterable<T>(iterable: AsyncIterable<T>): Promise<void> {
+export async function drainIterable<T>(iterable: AsyncIterable<T>, concurrency = 1): Promise<void> {
   let iterator = demandIterator(iterable);
-  while (true) {
-    let { done } = await iterator.next();
-    if (done) {
-      break;
-    }
+  let concurrentReads = new ConcurrentTasks(concurrency);
+  let done = false;
+
+  while (!done) {
+    await concurrentReads.waitForAvailability();
+    let promise = Promise.resolve(iterator.next()).then(isDone);
+    concurrentReads.add(promise);
+  }
+
+  function isDone(result: IteratorResult<T>) {
+    done = done || result.done === true;
   }
 }
--- test/specs/drain-terable.spec.js
+++ test/specs/drain-terable.spec.js
@@ -20,98 +20,102 @@ describe("drainIterable() function", () => {
     };
   }
 
-  it("should iterate over an empty iterable", async () => {
-    let spy = asyncIterableSpy([]);
-    let result = await drainIterable(spy);
+  for (let concurrency of [undefined, 1, 3, 5, 10]) {
+    describe(`concurrency: ${concurrency}`, () => {
 
-    expect(result).to.equal(undefined);
-    sinon.assert.calledOnce(spy[Symbol.asyncIterator]);
-    sinon.assert.calledOnce(spy.next);
-  });
+      it("should iterate over an empty iterable", async () => {
+        let spy = asyncIterableSpy([]);
+        let result = await drainIterable(spy, concurrency);
 
-  it("should iterate over a single-value iterable", async () => {
-    let spy = asyncIterableSpy([1]);
-    let result = await drainIterable(spy);
+        expect(result).to.equal(undefined);
+        sinon.assert.calledOnce(spy[Symbol.asyncIterator]);
+        expect(spy.next.callCount).to.be.at.least(1);
+      });
 
-    expect(result).to.equal(undefined);
-    sinon.assert.calledOnce(spy[Symbol.asyncIterator]);
-    sinon.assert.calledTwice(spy.next);
-  });
+      it("should iterate over a single-value iterable", async () => {
+        let spy = asyncIterableSpy([1]);
+        let result = await drainIterable(spy, concurrency);
 
-  it("should iterate over a multi-value iterable", async () => {
-    let spy = asyncIterableSpy([1, 2, 3]);
-    let result = await drainIterable(spy);
+        expect(result).to.equal(undefined);
+        sinon.assert.calledOnce(spy[Symbol.asyncIterator]);
+        expect(spy.next.callCount).to.be.at.least(2);
+      });
 
-    expect(result).to.equal(undefined);
-    sinon.assert.calledOnce(spy[Symbol.asyncIterator]);
-    sinon.assert.callCount(spy.next, 4);
-  });
+      it("should iterate over a multi-value iterable", async () => {
+        let spy = asyncIterableSpy([1, 2, 3]);
+        let result = await drainIterable(spy, concurrency);
 
-  it("should iterate over a non-async iterable", async () => {
-    await drainIterable([1, 2, 3]);
-  });
+        expect(result).to.equal(undefined);
+        sinon.assert.calledOnce(spy[Symbol.asyncIterator]);
+        expect(spy.next.callCount).to.be.at.least(4);
+      });
 
-  it("should throw an error if called with no arguments", async () => {
-    try {
-      await drainIterable();
-      assert.fail("An error should have been thrown");
-    }
-    catch (error) {
-      expect(error).to.be.an.instanceOf(TypeError);
-      expect(error.message).to.equal("Undefined is not iterable.");
-    }
-  });
+      it("should iterate over a non-async iterable", async () => {
+        await drainIterable([1, 2, 3], concurrency);
+      });
 
-  it("should throw an error if called with a non-iterable argument", async () => {
-    try {
-      await drainIterable(12345);
-      assert.fail("An error should have been thrown");
-    }
-    catch (error) {
-      expect(error).to.be.an.instanceOf(TypeError);
-      expect(error.message).to.equal("12345 is not iterable.");
-    }
-  });
+      it("should throw an error if called with no arguments", async () => {
+        try {
+          await drainIterable();
+          assert.fail("An error should have been thrown");
+        }
+        catch (error) {
+          expect(error).to.be.an.instanceOf(TypeError);
+          expect(error.message).to.equal("Undefined is not iterable.");
+        }
+      });
 
-  it("should throw an error if called with a Promise", async () => {
-    try {
-      await drainIterable(Promise.resolve([1, 2, 3]));
-      assert.fail("An error should have been thrown.");
-    }
-    catch (error) {
-      expect(error).to.be.an.instanceOf(TypeError);
-      expect(error.message).to.equal("A Promise is not iterable.");
-    }
-  });
+      it("should throw an error if called with a non-iterable argument", async () => {
+        try {
+          await drainIterable(12345);
+          assert.fail("An error should have been thrown");
+        }
+        catch (error) {
+          expect(error).to.be.an.instanceOf(TypeError);
+          expect(error.message).to.equal("12345 is not iterable.");
+        }
+      });
 
-  it("should throw an error if called with an invalid iterable", async () => {
-    try {
-      await drainIterable({ [Symbol.asyncIterator]: () => undefined });
-      assert.fail("An error should have been thrown");
-    }
-    catch (error) {
-      expect(error).to.be.an.instanceOf(TypeError);
-      expect(error.message).to.equal("{} is not iterable.");
-    }
-  });
+      it("should throw an error if called with a Promise", async () => {
+        try {
+          await drainIterable(Promise.resolve([1, 2, 3]));
+          assert.fail("An error should have been thrown.");
+        }
+        catch (error) {
+          expect(error).to.be.an.instanceOf(TypeError);
+          expect(error.message).to.equal("A Promise is not iterable.");
+        }
+      });
 
-  it("should propagate errors from iterables", async () => {
-    async function* source () {
-      yield 1;
-      await delay(50);
-      yield 2;
-      await delay(50);
-      throw new TypeError("Boom!");
-    }
+      it("should throw an error if called with an invalid iterable", async () => {
+        try {
+          await drainIterable({ [Symbol.asyncIterator]: () => undefined }, concurrency);
+          assert.fail("An error should have been thrown");
+        }
+        catch (error) {
+          expect(error).to.be.an.instanceOf(TypeError);
+          expect(error.message).to.equal("{} is not iterable.");
+        }
+      });
 
-    try {
-      await drainIterable(source());
-      assert.fail("An error should have been thrown.");
-    }
-    catch (error) {
-      expect(error).to.be.an.instanceOf(TypeError);
-      expect(error.message).to.equal("Boom!");
-    }
-  });
+      it("should propagate errors from iterables", async () => {
+        async function* source () {
+          yield 1;
+          await delay(50);
+          yield 2;
+          await delay(50);
+          throw new TypeError("Boom!");
+        }
 
+        try {
+          await drainIterable(source(), concurrency);
+          assert.fail("An error should have been thrown.");
+        }
+        catch (error) {
+          expect(error).to.be.an.instanceOf(TypeError);
+          expect(error.message).to.equal("Boom!");
+        }
+      });
+    });
+  }
 });
